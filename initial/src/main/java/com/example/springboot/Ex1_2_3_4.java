99f216ff6d556f5528b756681c86dab6d9d6685b
Roberto Tancredi <tancrediroberto07@gmail.com>
2023-06-05 13:30:07 +0200

    Ho unito gli esercizi 1-2-3-4

---
+++ initial/src/main/java/com/example/springboot/Ex1_2_3_4.java
@@ -0,0 +1,53 @@
+package com.example.springboot;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+import java.util.Random;
+
+@RestController
+public class Ex1_2_3_4 {
+
+
+    //ex1
+
+    @GetMapping(value = "/hello")
+    public String index() {
+        return "Hello World!";
+    }
+
+    //ex 2
+    @GetMapping(value = "/greeting")
+    public ResponseEntity<String> greeting() {
+        if (new Date().getHours() < 12) {
+            // esempio di errore 400 in caso sia mattina; (.status(400)e affini)
+            return ResponseEntity.badRequest().body("è mattina");
+        }
+        // in caso di stato 200 (o semplicemente .ok) caccia Buon pomeriggio
+        return ResponseEntity.status(200).body("Buon pomeriggio");
+        // return ResponseEntity.ok("Buon pomeriggio"); stesso metodo
+    }
+
+    //ex 3
+
+    @GetMapping(value = "/info")
+    public ResponseEntity info() {
+        return ResponseEntity.ok("200 OK");
+//      oppure
+//      return ResponseEntity.status(200).body("200 OK");
+    }
+
+    //ex 3
+    @GetMapping(value = "/random")
+    public ResponseEntity random() {
+        if (new Random().nextBoolean()) {
+            return ResponseEntity.ok("200 ok");
+
+        }
+        return ResponseEntity.badRequest().body("Errore 400");
+
+
+    }
+}
